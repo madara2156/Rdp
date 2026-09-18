01e9e2b5f87ee89c2ebec220121d9b9b380dbd08
madara2156 <tg9474439@gmail.com>
2026-09-18 09:18:51 +0530

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "[$(date)] Starting D-Bus..."
+mkdir -p /var/run/dbus
+rm -f /var/run/dbus/pid
+dbus-daemon --system --fork
+
+echo "[$(date)] Starting xrdp-sesman..."
+/usr/sbin/xrdp-sesman
+
+echo "[$(date)] Starting xrdp on port 3389..."
+/usr/sbin/xrdp --nodaemon &
+XRDP_PID=$!
+
+echo "[$(date)] xrdp is running (PID $XRDP_PID). Connect via Railway's TCP Proxy."
+
+wait "$XRDP_PID"
